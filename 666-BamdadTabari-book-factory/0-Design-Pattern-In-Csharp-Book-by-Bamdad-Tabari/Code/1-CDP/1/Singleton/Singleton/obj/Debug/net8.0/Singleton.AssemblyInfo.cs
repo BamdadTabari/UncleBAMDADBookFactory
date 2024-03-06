6875c18897efa718ca70c71dc146af4821a73f88
Mohammad Javad Tabari <mohammadJavadTabari1024@outlook.com>
2024-03-06 22:50:23 +0330

    1100101-I want convert myself to a computer program.

--- 666-BamdadTabari-book-factory/0-Design-Pattern-In-Csharp-Book-by-Bamdad-Tabari/Code/1-CDP/1/Singleton/Singleton/obj/Debug/net8.0/Singleton.AssemblyInfo.cs
+++ 666-BamdadTabari-book-factory/0-Design-Pattern-In-Csharp-Book-by-Bamdad-Tabari/Code/1-CDP/1/Singleton/Singleton/obj/Debug/net8.0/Singleton.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Singleton")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+13fa21a838131cd6ae4dd7dba50e794e35766661")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+bdee512c9088f5a9c76e842f6a795794507bff63")]
 [assembly: System.Reflection.AssemblyProductAttribute("Singleton")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Singleton")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
